6e1c6f6ae3a52913760909004ccc258bc86c2106
dzhygrynyuk <dzhygrynyuk@gmail.com>
2023-06-25 16:38:30 +0300

    Install Socket.IO for backend

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
-const app = express();
 const port = 3333;
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const rooms = new Map();
 
 app.get('/rooms', (req, res) => {
     res.json(rooms);
 })
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket}`);
+});
 
 app.listen(port, (err) => {
     if(err){
